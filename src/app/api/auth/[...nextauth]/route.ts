b7490e873eab8a6a2ce98152af683a007e9fa067
batooldev <batoohadeeqa@gmail.com>
2025-04-07 18:10:11 +0500

    remove errors

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -47,7 +47,7 @@ export const authOptions = {
     })
   ],
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user }: { token: any; user?: any }) {
       if (user) {
         token.id = user.id;
         token.name = user.name;
@@ -56,7 +56,7 @@ export const authOptions = {
       }
       return token;
     },
-    async session({ session, token }) {
+    async session({ session, token }: { session: any; token: any }) {
       if (session?.user) {
         session.user.id = token.id;
         session.user.name = token.name;
